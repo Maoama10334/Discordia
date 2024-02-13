1a1ca1f6280d35ad3c209c62cccbdde1176b62a0
Lukas9878 <a00286855@student.tus.ie>
2024-02-13 04:37:26 +0000

    Update main.js

--- Discordia/scripts/main.js
+++ Discordia/scripts/main.js
@@ -11,7 +11,7 @@ function createWindow() {
     }
   });
 
-  win.loadFile('index.html');
+  win.loadFile('C:/Users/LukasPC/Documents/Github/Discordia/Discordia/index.html');
 }
 
 app.whenReady().then(createWindow);
@@ -27,4 +27,41 @@ app.on('activate', () => {
     createWindow();
   }
 });
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+// MongoDB Code
+const { MongoClient } = require('mongodb');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'DiscordiaDB';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+
+  // Get the collections
+  const users = db.collection('users');
+  const servers = db.collection('servers');
+  const messages = db.collection('messages');
+
+  // Example: Insert a document into the users collection
+  const insertResult = await users.insertOne({ name: "John Doe", email: "john@example.com" });
+  console.log('Inserted document into the users collection', insertResult);
+
+  // Example: Find a document in the users collection
+  const findResult = await users.findOne({ name: "John Doe" });
+  console.log('Found document', findResult);
+
+  // Close connection
+  await client.close();
+}
+
+main().catch(console.error);
 
